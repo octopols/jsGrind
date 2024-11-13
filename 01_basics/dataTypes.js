c4716db685ba8a30d28d9f6ff18bbc705f1854b2
hirnaymay (harsh) <sony.vaio.1404@gmail.com>
2024-11-13 07:38:38 +0000

    sets container config and basics

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,16 @@
+let name = "Harsh"
+let age = "23"
+let isLoggedIn = "false"
+let state = null
+
+// number =< 2^53
+// bigint
+// string " "
+// boolean -> true / false
+// null -> standalone value
+// undefined undifined
+
+// objects
+
+console.log(typeof null); // returns object 
+console.log(typeof undefined); // returns undefined
---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = "7017007660";
+let accountEmail = "hirnaymay@gmail.com";
+var accountPassword = "12345";
+accountCity = "Delhi";
+let accountState;
+
+accountEmail = "hb@hb.com";
+accountPassword = "0000";
+accountCity = "Bengaluru";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
